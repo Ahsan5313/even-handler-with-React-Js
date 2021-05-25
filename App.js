4d0d0ba4ae1983381606a7269ad6367632dea32b
Freelancer Ahsan <81156921+Ahsan5313@users.noreply.github.com>
2021-05-25 22:00:56 +0600

    add React Js for the project

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import STATE from './STATE';
+import Conditional_Rendering from './Conditional_Rendering/index'
+import Even_Handler from './Even_Handler/index'
+
+export default function App() {
+  return (
+    <div>
+     
+     <Even_Handler />
+    </div>
+  )
+}
